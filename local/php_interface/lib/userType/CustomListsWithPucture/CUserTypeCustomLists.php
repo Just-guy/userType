65be90f257a3f12ac07cc2bc5f1c83a52adf734d
Just-guy <62421873+Just-guy@users.noreply.github.com>
2025-03-07 13:25:43 +0300

    Доработка функционала
    1. Подключил библиотеку для drag'b'drop'а элементов;
    2. Добавил головной блок с иконкой drag'n'drop, кнопкой для сворачивания и удаления элемента;

--- local/php_interface/lib/userType/CustomListsWithPucture/CUserTypeCustomLists.php
+++ local/php_interface/lib/userType/CustomListsWithPucture/CUserTypeCustomLists.php
@@ -30,7 +30,7 @@ class CUserTypeCustomLists extends \Bitrix\Main\UserField\Types\StringType
 		$arHtmlControl["VALUE"] = unserialize(htmlspecialchars_decode($arHtmlControl["VALUE"]));
 
 		preg_match('/\[([0-9]+)\]/', $arHtmlControl['NAME'], $matches);
- 		$keySection = $matches[1];
+		$keySection = $matches[1];
 		$keyElement = 0;
 		$arHtmlControl['NAME'] = $arHtmlControl['NAME'] . '[ICON]';
 
@@ -47,7 +47,11 @@ class CUserTypeCustomLists extends \Bitrix\Main\UserField\Types\StringType
 		// === File
 
 		$html .= '<div class="custom-lists__group' . ($condition == 'close' ? ' custom-lists__group_close' : '') . '">';
+		$html .= '<div class="custom-lists__header-block">';
+		$html .= '<span class="custom-lists__drag-over"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M278.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l9.4-9.4L224 224l-114.7 0 9.4-9.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-9.4-9.4L224 288l0 114.7-9.4-9.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-9.4 9.4L288 288l114.7 0-9.4 9.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l9.4 9.4L288 224l0-114.7 9.4 9.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-64-64z"/></svg></span>';
 		$html .= '<a href onclick="changeOfState(event, ' . $keySection . ', `' . $arUserField["FIELD_NAME"] . '`)">' . $conditionButton . '</a>';
+		$html .= '<input class="custom-lists__delete-node" type="button" value="x" onclick="deleteNode(this.closest(`.custom-lists`))">';
+		$html .= '</div>';
 		$html .= '<div class="custom-lists__top-block">';
 		$html .= '<div class="custom-lists__image-block">';
 
@@ -70,7 +74,6 @@ class CUserTypeCustomLists extends \Bitrix\Main\UserField\Types\StringType
 		$html .= '<div class="custom-lists__section-input">';
 		$html .= '<input class="custom-lists__section-name" type="text" name="' . $arUserField["FIELD_NAME"] . '[' . $keySection . '][TITLE_SECTION]" value="' . $arHtmlControl["VALUE"]["TITLE_SECTION"] . '" placeholder="Название раздела" size="30">';
 		$html .= '<input class="custom-lists__section-url" type="text" name="' . $arUserField["FIELD_NAME"] . '[' . $keySection . '][URL_SECTION]" value="' . $arHtmlControl["VALUE"]["URL_SECTION"] . '" placeholder="Ссылка для раздела" size="30">';
-		$html .= '<input class="custom-lists__delete-node" type="button" value="x" onclick="deleteNode(this.closest(`.custom-lists`))">';
 		$html .= '</div>';
 		$html .= '</div>';
 
@@ -94,9 +97,8 @@ class CUserTypeCustomLists extends \Bitrix\Main\UserField\Types\StringType
 
 		$html .= '</div>';
 
-		return $html; ?>
-
-<? }
+		return $html;
+	}
 
 	public static function OnBeforeSave($arUserField, $value)
 	{
@@ -186,7 +188,7 @@ class CUserTypeCustomLists extends \Bitrix\Main\UserField\Types\StringType
 }
 
 ?>
-
+<script src="/node/node_modules/sortablejs/Sortable.js"></script>
 <script>
 	BX.bind(document, 'click', function(event) {
 		if (BX.hasClass(event.target, 'custom-lists__delete-node')) deleteNode(event.target.closest('.custom-lists__element'));
@@ -250,7 +252,6 @@ class CUserTypeCustomLists extends \Bitrix\Main\UserField\Types\StringType
 	}
 
 	function changeOfState(event, keySection, fieldName) {
-		//debugger
 		event.preventDefault();
 		let customlistsGroup = event.target.closest('.custom-lists__group'),
 			condition = document.querySelector('[name="' + fieldName + '[' + keySection + '][CONDITION]"]').value;
@@ -259,107 +260,159 @@ class CUserTypeCustomLists extends \Bitrix\Main\UserField\Types\StringType
 		event.target.text = (condition == 'close' ? "Свернуть" : "Развернуть");
 		document.querySelector('[name="' + fieldName + '[' + keySection + '][CONDITION]"]').value = (condition == 'close' ? "open" : "close");
 	}
+
+	document.addEventListener('DOMContentLoaded', function() {
+		new Sortable(document.querySelector('#table_UF_CUSTOM_LISTS > tbody'), {
+			animation: 150,
+			easing: "cubic-bezier(1, 0, 0, 1)",
+			handle: ".custom-lists__drag-over",
+		})
+	})
 </script>
-
 <style>
-	.custom-lists {
-		margin-bottom: 20px;
-	}
+.custom-lists {
+	margin-bottom: 20px;
+}
 
-	.custom-lists__group {
-		display: flex;
-		align-items: normal;
-		flex-direction: column;
-		padding: 20px;
-		border: 1px solid #dddddd;
-		border-radius: 3px;
-		background-color: #f7f7f7;
-		gap: 15px;
-		transition: .2s ease-out;
-	}
+.custom-lists__group {
+	display: flex;
+	align-items: normal;
+	flex-direction: column;
+	padding: 10px;
+	border: 1px solid #dddddd;
+	border-radius: 3px;
+	background-color: #f7f7f7;
+	gap: 15px;
+	transition: .2s ease-out;
+}
 
-	.custom-lists__group.custom-lists__group_close {
-		padding: 10px;
-		gap: 5px;
-	}
+.custom-lists__group.custom-lists__group_close {
+	padding: 10px;
+	gap: 5px;
+}
 
-	.custom-lists__top-block {
-		display: flex;
-		align-items: center;
-		gap: 20px;
-	}
+.custom-lists__top-block {
+	display: flex;
+	align-items: center;
+	gap: 20px;
+}
 
-	.custom-lists__image-block {
-		display: flex;
-		gap: 10px;
-		flex: 1;
-	}
+.custom-lists__image-block {
+	display: flex;
+	gap: 10px;
+	flex: 1;
+}
 
-	.custom-lists__image>img {
-		max-width: 100px;
-	}
+.custom-lists__image>img {
+	max-width: 100px;
+}
 
-	.custom-lists__image {
-		flex-basis: 100px;
-		align-items: center;
-		display: flex;
-		justify-content: center;
-	}
+.custom-lists__image {
+	flex-basis: 100px;
+	align-items: center;
+	display: flex;
+	justify-content: center;
+}
 
-	.custom-lists__image-data {
-		display: flex;
-		flex-direction: column;
-		align-items: baseline;
-	}
+.custom-lists__image-data {
+	display: flex;
+	flex-direction: column;
+	align-items: baseline;
+}
 
-	.custom-lists__image-block .bx-input-file-desc {
-		word-break: break-all;
-	}
+.custom-lists__image-block .bx-input-file-desc {
+	word-break: break-all;
+}
 
-	.custom-lists__group.custom-lists__group_close .custom-lists__section-input {
-		margin: 0;
-	}
+.custom-lists__group.custom-lists__group_close .custom-lists__section-input {
+	margin: 0;
+}
 
-	.custom-lists__section-input {
-		margin-bottom: 5px;
-		display: flex;
-		align-items: center;
-		transition: .2s ease-out;
-	}
+.custom-lists__section-input {
+	margin-bottom: 5px;
+	display: flex;
+	align-items: center;
+	transition: .2s ease-out;
+}
 
-	input.custom-lists__section-name,
-	input.custom-lists__section-url {
-		flex: 1;
-	}
+input.custom-lists__section-name,
+input.custom-lists__section-url {
+	flex: 1;
+}
 
-	.custom-lists__middle-block>.custom-lists__section-input>.custom-lists__section-name,
-	.custom-lists__list-elements>.custom-lists__element>.custom-lists__element-name {
-		margin-right: 5px;
-	}
+.custom-lists__middle-block>.custom-lists__section-input>.custom-lists__section-name,
+.custom-lists__list-elements>.custom-lists__element>.custom-lists__element-name {
+	margin-right: 5px;
+}
 
-	.custom-lists__list-elements {
-		display: flex;
-		justify-content: end;
-		flex-direction: column;
-		align-items: end;
-	}
+.custom-lists__list-elements {
+	display: flex;
+	justify-content: end;
+	flex-direction: column;
+	align-items: end;
+}
 
-	.custom-lists__element {
-		width: 85%;
-		display: flex;
-		align-items: center;
-	}
+.custom-lists__element {
+	width: 85%;
+	display: flex;
+	align-items: center;
+	margin-bottom: 5px;
+}
 
-	.custom-lists__element input[type="text"] {
-		width: 100%;
-	}
+.custom-lists__element input[type="text"] {
+	width: 100%;
+}
 
-	.custom-lists__group input[type="button"].custom-lists__delete-node {
-		margin: 0 0 0 10px;
-	}
+.custom-lists__group input[type="button"].custom-lists__delete-node {
+	margin: 0 0 0 10px;
+}
 
-	.custom-lists__group_close>.custom-lists__top-block,
-	.custom-lists__group_close>.custom-lists__bottom-block {
-		display: none;
-	}
+.custom-lists__group_close>.custom-lists__top-block,
+.custom-lists__group_close>.custom-lists__bottom-block {
+	display: none;
+}
+
+.custom-lists__header-block {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-bottom: 10px;
+}
+
+.custom-lists>.custom-lists__group>.custom-lists__header-block>.custom-lists__delete-node {
+	color: #fff;
+	border: none;
+	box-shadow: none;
+	transition: .2s ease-out;
+	background-color: #bf3737 !important;
+	background-image: none !important;
+	font-weight: normal;
+	height: 20px;
+	font-size: 10px;
+	padding: 0px 7px;
+	line-height: 20px;
+}
+
+.custom-lists>.custom-lists__group>.custom-lists__header-block>.custom-lists__delete-node:hover {
+	background-color: #df6c6c !important;
+}
+
+.custom-lists__drag-over {
+	height: 20px;
+	padding: 0px 4px;
+	background-color: #cbcbcb;
+	display: inline-flex;
+	align-items: center;
+	border-radius: 4px;
+	transition: .2s ease-out;
+	cursor: pointer;
+}
+
+.custom-lists__drag-over:hover {
+	background-color: #ababab;
+}
+
+.custom-lists__drag-over > svg {
+	height: 12px;
+}
 </style>
