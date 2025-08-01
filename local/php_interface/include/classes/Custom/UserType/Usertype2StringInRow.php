6187d04ce96347cfc58f3c5129fcee1e33b41767
Just-guy <62421873+Just-guy@users.noreply.github.com>
2025-08-01 16:45:09 +0300

    Добавил библиотеку sortableJS

--- local/php_interface/include/classes/Custom/UserType/Usertype2StringInRow.php
+++ local/php_interface/include/classes/Custom/UserType/Usertype2StringInRow.php
@@ -1,6 +1,6 @@
 <?
 
-namespace Custom\UserType;
+namespace Custom\Usertype;
 
 use Bitrix\Main\Text\HtmlFilter;
 use Bitrix\Main\Loader,
@@ -9,7 +9,7 @@ use Bitrix\Main\Loader,
 
 /**
  * Class Usertype2StringInRow
- * @package Custom\UserType
+ * @package lib\usertype
  */
 
 class Usertype2StringInRow extends \Bitrix\Main\UserField\Types\StringType
@@ -26,8 +26,13 @@ class Usertype2StringInRow extends \Bitrix\Main\UserField\Types\StringType
 	}
 
 	public static function GetEditFormHTML(array $arUserField, ?array $arHtmlControl): string
-	{
-		$firstEmpty = true;
+	{ ?>
+
+		<script>
+			initializingSortable("<?= $arUserField["FIELD_NAME"] ?>")
+		</script>
+
+<? $firstEmpty = true;
 
 		$arHtmlControl["VALUE"] = unserialize(htmlspecialchars_decode($arHtmlControl['VALUE']));
 
@@ -35,11 +40,12 @@ class Usertype2StringInRow extends \Bitrix\Main\UserField\Types\StringType
 		$value = (!empty($arHtmlControl["VALUE"]["VALUE"]) ? $arHtmlControl["VALUE"]["VALUE"] : '');
 
 		$html =  '<div class="two-string-in-row">';
+		$html .= '<span class="two-string-in-row__drag-over"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M278.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l9.4-9.4L224 224l-114.7 0 9.4-9.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-9.4-9.4L224 288l0 114.7-9.4-9.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-9.4 9.4L288 288l114.7 0-9.4 9.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l9.4 9.4L288 224l0-114.7 9.4 9.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-64-64z"/></svg></span>';
 		$html .= '<div class="two-string-in-row__field">';
-		$html .= '<input type="text" name="' . $arHtmlControl["NAME"] . '[TITLE]' . '" value="' . $title . '" placeholder="Заголовок" size="60"><br>';
+		$html .= '<input type="text" name="' . $arHtmlControl["NAME"] . '[TITLE]' . '" value="' . $title . '" placeholder="Название" size="60"><br>';
 		$html .= '</div>';
 		$html .= '<div class="two-string-in-row__field">';
-		$html .= '<input type="text" name="' . $arHtmlControl["NAME"] . '[VALUE]' . '" value="' . $value . '" placeholder="Значение" size="60"><br>';
+		$html .= '<input type="text" name="' . $arHtmlControl["NAME"] . '[VALUE]' . '" value="' . $value . '" placeholder="Ссылка" size="60"><br>';
 		$html .= '</div>';
 		$html .= '</div>';
 
@@ -121,6 +127,18 @@ class Usertype2StringInRow extends \Bitrix\Main\UserField\Types\StringType
 	}
 }
 ?>
+<script src="../../local/php_interface/node/node_modules/sortablejs/Sortable.min.js"></script>
+<script>
+	function initializingSortable($fieldName) {
+		document.addEventListener('DOMContentLoaded', function() {
+			new Sortable(document.querySelector('#table_' + $fieldName + ' > tbody'), {
+				animation: 150,
+				easing: "cubic-bezier(1, 0, 0, 1)",
+				handle: ".two-string-in-row__drag-over",
+			})
+		});
+	}
+</script>
 <style>
 	.two-string-in-row {
 		background-color: #ececed;
@@ -130,6 +148,7 @@ class Usertype2StringInRow extends \Bitrix\Main\UserField\Types\StringType
 		max-width: 600px;
 		border-radius: 5px;
 		border: 1px solid #dfdfdf;
+		align-items: center;
 	}
 
 	.two-string-in-row__field {
@@ -138,5 +157,24 @@ class Usertype2StringInRow extends \Bitrix\Main\UserField\Types\StringType
 
 	.two-string-in-row__field input[type="text"] {
 		width: 100%;
+	}
+
+	.two-string-in-row__drag-over {
+		height: 20px;
+		background-color: rgb(203, 203, 203);
+		display: inline-flex;
+		align-items: center;
+		cursor: pointer;
+		padding: 0px 4px;
+		border-radius: 4px;
+		transition: 0.2s ease-out;
+	}
+
+	.two-string-in-row__drag-over:hover {
+		background-color: #ababab;
+	}
+
+	.two-string-in-row__drag-over>svg {
+		height: 12px;
 	}
 </style>
